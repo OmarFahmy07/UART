cacc209c90d1b86349f28c47b4e496779c862a0a
Omar Fahmy <omar.ahmed.fahmy2000@gmail.com>
2022-08-10 21:14:31 +0200

    Add files via upload

--- UART Tx/MUX_8x1.v
+++ UART Tx/MUX_8x1.v
@@ -1,12 +1,24 @@
-module MUX_8x1(
-  input wire [2:0] sel,
-  input wire [7:0] data,
-  output reg out
-  );
-              
-  always@(*)
+module MUX_8x1(input wire clk,
+               input wire rst,
+               input wire [2:0] sel,
+               input wire [7:0] data,
+               output reg out);
+    
+    wire out_comb;
+    
+    always@(posedge clk or negedge rst)
     begin
-      out = data[sel];
+        if (!rst)
+        begin
+            // IDLE bit is high
+            out <= 1'b1;
+        end
+        else
+        begin
+            out <= out_comb;
+        end
     end
-  
+    
+    assign out_comb = data[sel];
+    
 endmodule
